6574f9079f037683f212911ba68f804bbffd2dec
Candy-Chebet <chebetcandychebet@gmail.com>
2023-05-10 23:16:37 -0700

    Completed Number Guessing Game

--- Quiz_Games/numberGuessing_Game.py
+++ Quiz_Games/numberGuessing_Game.py
@@ -15,3 +15,24 @@ else:
         
         
 random_number = random.randint(0, top_of_range)
+guesses = 0;
+while True:
+    guesses += 1
+    user_guess = input("Make a guess! ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+        print("Please enter a number next time")
+        continue
+        
+    if user_guess == random_number:
+       print("You got it right!")
+       break
+    elif user_guess > random_number:
+            print("You were above the number")
+    else:
+            print("You were below the number")
+
+
+print("You got it in", + guesses, "guesses")
+       
